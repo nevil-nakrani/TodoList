8f44f07aea34df3d4d74f1667e66a96c6f1dd4f8
Nevil-Nakrani <nakaraninevil2017@gmail.com>
2024-10-04 00:14:54 +0530

    Succefully, Project is completed

---
+++ src/context/TodoContext.js
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [{ id: 1, todo: "Todo Message", completed: false }],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const TodoProvider = TodoContext.Provider;
+
+export function useTodo() {
+  return useContext(TodoContext);
+}
